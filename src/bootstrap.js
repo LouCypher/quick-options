b7dd5ade4a44df6ec86f6c2d0e15efbec4385c2f
LouCypher <zoolcar9+dev@gmail.com>
2013-11-09 21:50:16 +0700

    Refactored

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -16,8 +16,7 @@ function log(aString) {
   Services.console.logStringMessage("Bootstrap:\n" + aString);
 }
 
-function setResourceName(aData) aData.id.toLowerCase().match(/[^\@]+/).toString()
-                                                      .replace(/[^\w]/g, "");
+let RESOURCE_NAME;
 
 function resProtocolHandler(aResourceName, aURI) {
   io.getProtocolHandler("resource")
@@ -25,134 +24,134 @@ function resProtocolHandler(aResourceName, aURI) {
     .setSubstitution(aResourceName, aURI, null);
 }
 
-function openPreferencesInTab(aWindow, aPaneId) {
-  function aboutPreferences(aBrowser, aPaneId) {
-    let win = aBrowser.contentWindow;
-    let gotoPref = win.wrappedJSObject.gotoPref;
-    gotoPref(aPaneId)
-    win.focus();
-  }
-
-  // This will switch to the tab in aWindow having aURI, if present.
-  function switchIfURIInWindow(aWindow, aURI) {
-    let browsers = aWindow.gBrowser.browsers;
-    for (let i = 0; i < browsers.length; i++) {
-      let browser = browsers[i];
-      if (browser.currentURI.equals(aURI)) {
-        // Focus the matching window & tab
-        aWindow.focus();
-        aWindow.gBrowser.tabContainer.selectedIndex = i;
-        return true;
-      }
-    }
-    return false;
-  }
-
-  let URI = "about:preferences";
-
-  // Bug 767313
-  if (aPaneId == "paneTabs" && Services.appinfo.version > "25")
-    aPaneId = "paneGeneral";
-
-  // This can be passed either nsIURI or a string.
-  if (!(URI instanceof Ci.nsIURI))
-    URI = io.newURI(URI, null, null);
-
-  let isBrowserWindow = !!aWindow.gBrowser;
-
-  // Prioritise this window.
-  if (isBrowserWindow && switchIfURIInWindow(aWindow, URI)) {
-    aboutPreferences(aWindow.getBrowser().selectedBrowser, aPaneId);
-    return;
-  }
-
-  let winEnum = wm.getEnumerator("navigator:browser");
-  while (winEnum.hasMoreElements()) {
-    let browserWin = winEnum.getNext();
-    // Skip closed (but not yet destroyed) windows,
-    // and the current window (which was checked earlier).
-    if (browserWin.closed || browserWin == aWindow) {
-      aWindow.openLinkIn(URI.spec, "tab", {fromChrome:true});
-      browserWin.addEventListener("pageshow", function browserWinPageShow(event) {
-        if (event.target.location.href != URI.spec)
-          return;
-        browserWin.removeEventListener("pageshow", browserWinPageShow, true);
-        aboutPreferences(browserWin.getBrowser().selectedBrowser, aPaneId);
-      }, true)
-    }
-    if (switchIfURIInWindow(browserWin, URI))
-      return;
-  }
-}
-
-function openPreferences(aWindow, aPaneId, aExtraArgs) {
-  let prefBranch = prefs.getBranch("browser.preferences.");
-
-  if (prefBranch.getBoolPref("inContent") && aPaneId != "paneDownloads")
-    openPreferencesInTab(aWindow, aPaneId);
-
-  else {
-    if (aPaneId == "paneGeneral")
-      aPaneId = "paneMain";
-
-    let instantApply = prefBranch.getBoolPref("instantApply", false);
-    let features = "chrome, titlebar, toolbar, centerscreen" +
-                   (instantApply ? ", dialog=no" : ", modal");
-
-    let win = wm.getMostRecentWindow("Browser:Preferences");
-    if (win) {
-      win.focus();
-      if (aPaneId) {
-        let pane = win.document.getElementById(aPaneId);
-        win.document.documentElement.showPane(pane);
-      }
-
-      if (aExtraArgs && aExtraArgs["advancedTab"]) {
-        let advancedPaneTabs = win.document.getElementById("advancedPrefs");
-        advancedPaneTabs.selectedTab = win.document.getElementById(aExtraArgs["advancedTab"]);
-      }
-
-     return;
-    }
-
-    aWindow.openDialog("chrome://browser/content/preferences/preferences.xul",
-                       "Preferences", features, aPaneId, aExtraArgs);
-  }
-}
-
-function addMenuitem(aWindow, aLabel, aPaneId) {
-  let menuitem = aWindow.document.createElement("menuitem");
-  menuitem.className = "quick-options";
-  menuitem.setAttribute("label", aLabel);
-  menuitem.setAttribute("value", aPaneId);
-  //menuitem.setAttribute("oncommand", "openPreferences(this.value);"); // AMO doesn't like this
-  menuitem.addEventListener("command", function(aEvent) { // AMO prefer this
-    let paneId = aEvent.target.value;
-    openPreferences(aWindow, paneId);
-  })
-  return menuitem;
-}
-
-function getPaneName(aPaneId, aString) {
-  try {
-    return aString.match("ENTITY.*" + aPaneId + "[^\>]+").toString()
-                  .match(/\".*/).toString()
-                  .replace(/\"/g, "");
-  } catch(ex) {
-    return aPaneId;
-  }
-}
-
-function getStringsFromDTD(aChromeURL) {
-  const XMLHttpRequest = CC("@mozilla.org/xmlextras/xmlhttprequest;1", "nsIXMLHttpRequest");
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", aChromeURL, false);
-  xhr.send(null);
-  return xhr.responseText;
-}
-
 function quickOptionsInit(aWindow) {
   const {document} = aWindow;
+
+  function openPreferencesInTab(aWindow, aPaneId) {
+    function aboutPreferences(aBrowser, aPaneId) {
+      let win = aBrowser.contentWindow;
+      let gotoPref = win.wrappedJSObject.gotoPref;
+      gotoPref(aPaneId)
+      win.focus();
+    }
+
+    // This will switch to the tab in aWindow having aURI, if present.
+    function switchIfURIInWindow(aWindow, aURI) {
+      let browsers = aWindow.gBrowser.browsers;
+      for (let i = 0; i < browsers.length; i++) {
+        let browser = browsers[i];
+        if (browser.currentURI.equals(aURI)) {
+          // Focus the matching window & tab
+          aWindow.focus();
+          aWindow.gBrowser.tabContainer.selectedIndex = i;
+          return true;
+        }
+      }
+      return false;
+    }
+
+    let URI = "about:preferences";
+
+    // Bug 767313
+    if (aPaneId == "paneTabs" && Services.appinfo.version > "25")
+      aPaneId = "paneGeneral";
+
+    // This can be passed either nsIURI or a string.
+    if (!(URI instanceof Ci.nsIURI))
+      URI = io.newURI(URI, null, null);
+
+    let isBrowserWindow = !!aWindow.gBrowser;
+
+    // Prioritise this window.
+    if (isBrowserWindow && switchIfURIInWindow(aWindow, URI)) {
+      aboutPreferences(aWindow.getBrowser().selectedBrowser, aPaneId);
+      return;
+    }
+
+    let winEnum = wm.getEnumerator("navigator:browser");
+    while (winEnum.hasMoreElements()) {
+      let browserWin = winEnum.getNext();
+      // Skip closed (but not yet destroyed) windows,
+      // and the current window (which was checked earlier).
+      if (browserWin.closed || browserWin == aWindow) {
+        aWindow.openLinkIn(URI.spec, "tab", {fromChrome:true});
+        browserWin.addEventListener("pageshow", function browserWinPageShow(event) {
+          if (event.target.location.href != URI.spec)
+            return;
+          browserWin.removeEventListener("pageshow", browserWinPageShow, true);
+          aboutPreferences(browserWin.getBrowser().selectedBrowser, aPaneId);
+        }, true)
+      }
+      if (switchIfURIInWindow(browserWin, URI))
+        return;
+    }
+  }
+
+  function openPreferences(aWindow, aPaneId, aExtraArgs) {
+    let prefBranch = prefs.getBranch("browser.preferences.");
+
+    if (prefBranch.getBoolPref("inContent") && aPaneId != "paneDownloads")
+      openPreferencesInTab(aWindow, aPaneId);
+
+    else {
+      if (aPaneId == "paneGeneral")
+        aPaneId = "paneMain";
+
+      let instantApply = prefBranch.getBoolPref("instantApply", false);
+      let features = "chrome, titlebar, toolbar, centerscreen" +
+                     (instantApply ? ", dialog=no" : ", modal");
+
+      let win = wm.getMostRecentWindow("Browser:Preferences");
+      if (win) {
+        win.focus();
+        if (aPaneId) {
+          let pane = win.document.getElementById(aPaneId);
+          win.document.documentElement.showPane(pane);
+        }
+
+        if (aExtraArgs && aExtraArgs["advancedTab"]) {
+          let advancedPaneTabs = win.document.getElementById("advancedPrefs");
+          advancedPaneTabs.selectedTab = win.document.getElementById(aExtraArgs["advancedTab"]);
+        }
+
+       return;
+      }
+
+      aWindow.openDialog("chrome://browser/content/preferences/preferences.xul",
+                         "Preferences", features, aPaneId, aExtraArgs);
+    }
+  }
+
+  function addMenuitem(aWindow, aLabel, aPaneId) {
+    let menuitem = aWindow.document.createElement("menuitem");
+    menuitem.className = "quick-options";
+    menuitem.setAttribute("label", aLabel);
+    menuitem.setAttribute("value", aPaneId);
+    //menuitem.setAttribute("oncommand", "openPreferences(this.value);"); // AMO doesn't like this
+    menuitem.addEventListener("command", function(aEvent) { // AMO prefer this
+      let paneId = aEvent.target.value;
+      openPreferences(aWindow, paneId);
+    })
+    return menuitem;
+  }
+
+  function getPaneName(aPaneId, aString) {
+    try {
+      return aString.match("ENTITY.*" + aPaneId + "[^\>]+").toString()
+                    .match(/\".*/).toString()
+                    .replace(/\"/g, "");
+    } catch(ex) {
+      return aPaneId;
+    }
+  }
+
+  function getStringsFromDTD(aChromeURL) {
+    const XMLHttpRequest = CC("@mozilla.org/xmlextras/xmlhttprequest;1", "nsIXMLHttpRequest");
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", aChromeURL, false);
+    xhr.send(null);
+    return xhr.responseText;
+  }
 
   let prefDTD = getStringsFromDTD("chrome://browser/locale/preferences/preferences.dtd");
 
@@ -225,13 +224,12 @@ function quickOptionsInit(aWindow) {
  * Handle the add-on being activated on install/enable
  */
 function startup(data, reason) {
-
   // Add `resource:` alias
-  let resourceName = setResourceName(data);
-  //log(resourceName);
-  resProtocolHandler(resourceName, data.resourceURI);
+  RESOURCE_NAME = data.id.toLowerCase().match(/[^\@]+/).toString().replace(/[^\w]/g, "");
+  //log(RESOURCE_NAME);
+  resProtocolHandler(RESOURCE_NAME, data.resourceURI);
 
-  Cu.import("resource://" + resourceName + "/watchwindows.jsm");
+  Cu.import("resource://" + RESOURCE_NAME + "/watchwindows.jsm");
   watchWindows(quickOptionsInit);
 }
 
@@ -244,9 +242,8 @@ function shutdown(data, reason) {
     return;
 
   unload();
-  let resourceName = setResourceName(data);
-  Cu.unload("resource://" + resourceName + "/watchwindows.jsm");
-  resProtocolHandler(resourceName, null); // Remove `resource:` alias
+  Cu.unload("resource://" + RESOURCE_NAME + "/watchwindows.jsm");
+  resProtocolHandler(RESOURCE_NAME, null); // Remove `resource:` alias
 }
 
 /**
